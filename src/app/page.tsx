6366e2d1889789a8b88f47784f786ff429eabb88
“tsebtseb” <tsetseb.alone@gmal.com>
2025-03-13 00:46:27 -0700

    fix: improve clarity of salary comparison description in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -953,7 +953,7 @@ export default function Home() {
           <Card>
       <CardHeader>
         <CardTitle>Salary Comparison</CardTitle>
-        <CardDescription>show comparison of Total earning per year as line chart</CardDescription>
+        <CardDescription>show a comparison of the user's salary versus the job salary for each year as a line chart.</CardDescription>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
