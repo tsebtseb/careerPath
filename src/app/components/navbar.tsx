ec98ea689adafee1ec7fd44fcad04e2ea85a6c78
“tsebtseb” <tsetseb.alone@gmal.com>
2025-04-02 13:13:23 +0700

    fix: add logo image to navbar for improved branding

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,24 +1,29 @@
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+import EscapeLogo from "/public/escape path.png";
 
 import Link from "next/link";
-export default function Navbar(){
-    return(
-        <div className="bg-white/10 backdrop-blur-md p-4 pt-4 pb-3.5 border border-white/20 shadow-2xl rounded-xl mx-8 mb-8">
-        <div className="container flex justify-between items-center mx-auto">
-          <Link href="/">
+export default function Navbar() {
+  return (
+    <div className="bg-white/10 backdrop-blur-md p-4 pt-4 pb-3.5 border border-white/20 shadow-2xl rounded-xl mx-8 mb-8">
+      <div className="container flex justify-between items-center mx-auto">
+        <Link href="/">
+          <div className="flex space-x-4 items-center">
+            <Image src={EscapeLogo} alt="logo" className="w-12 h-12" />
             <h1 className="text-2xl tracking-wide font-black text-black">
               Career Path
             </h1>
-          </Link>
-          <Link href="/datatable">
+          </div>
+        </Link>
+        <Link href="/datatable">
           <button className="flex items-center bg-gradient-to-r from-[#a13ddd] to-[#ee3aac] shadow-2xl  text-white p-4 m-4 rounded-3xl hover:from-purple-800 hover:to-pink-600 hover:text-gray-100">
             <h2 className="font-bold">Data Table</h2>
             <span>
               <ArrowTopRightOnSquareIcon className="icon w-8 h-8 ml-2" />
             </span>
           </button>
-          </Link>
-        </div>
+        </Link>
       </div>
-    )
+    </div>
+  );
 }
